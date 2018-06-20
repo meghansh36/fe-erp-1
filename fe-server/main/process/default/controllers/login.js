ba65ff1bfbd0250aa63ac672479999d1f442c777
Chirag <Chirag2120787@gmail.com>
2018-06-20 19:17:25 +0530

    login server

---
+++ fe-server/main/process/default/controllers/login.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+passport.use(new LocalStrategy(
+      {usernameField:"user", passwordField:"pass"},
+        function (username, password, done) {
+        if (username === 'foo' && password === 'bar') {
+            done(null, true);
+        } else {
+            done(null, false);
+        }
+    }));
+    
+module.exports = passport.initialize();
