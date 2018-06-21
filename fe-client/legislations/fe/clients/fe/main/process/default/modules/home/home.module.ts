3e503d797187621301a9989e7b97880b53e40c88
mavericshubham <maveric.shubham@gmail.com>
2018-06-21 06:21:05 +0000

    Git Commit from Server 21-Jun-2018

--- fe-client/legislations/fe/clients/fe/main/process/default/modules/home/home.module.ts
+++ fe-client/legislations/fe/clients/fe/main/process/default/modules/home/home.module.ts
@@ -1,27 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LgHomeModule } from '@L2Process/defualt/modules/home/home.module';
 
-import { HomeComponent } from '@L3Process/default/modules/home/home.component';
-
-import { HomesModule } from './homes/homes.module';
-import { FormBuilderModule } from '@L3Process/system/modules/formBuilder/formBuilder.module';
-import { FormRenderModule } from '@L3Process/system/modules/formRender/formRender.module';
-import { routes } from '@L3Process/default/modules/home/home.routing';
-
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routing: ModuleWithProviders = RouterModule.forRoot(routes);
-
-@NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    routing
-  ],
-  providers: [],
-  bootstrap: [HomeComponent]
-})
 export class HomeModule { }
--- fe-client/main/process/default/modules/home/home.module.ts
+++ fe-client/main/process/default/modules/home/home.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HomeComponent } from '@L3Process/default/modules/home/home.component';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from '@L3Process/default/modules/home/home.component';
+import { FormBuilderModule } from '@L3Process/system/modules/formBuilder/formBuilder.module';
+import { FormRenderModule } from '@L3Process/system/modules/formRender/formRender.module';
+import { routes } from '@L3Process/default/modules/home/home.routing';
+
+const routing: ModuleWithProviders = RouterModule.forRoot(routes);
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [HomeComponent]
