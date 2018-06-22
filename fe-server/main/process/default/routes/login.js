54737cbe60da8c4ab8452351f6715ffbd47448c8
Chirag <Chirag2120787@gmail.com>
2018-06-22 11:47:34 +0530

    login server

--- fe-server/main/process/default/routes/login.js
+++ fe-server/main/process/default/routes/login.js
@@ -2,36 +2,45 @@ var express = require('express');
 var router = express.Router();
 const passport = require('passport');
 
+// router.post('/login',
+//   passport.authenticate('local'),
+//   function(req, res) {
+//     res.json({
+//         success:true,
+//         message: 'Login seuccessful'
+//     });
+//   }
+// );
 
-// router.post('/login', (req, res, next) => {
-//     passport.authenticate('local', (err) => {
-//         {
-//             res.json({
-//                 success: true,
-//                 message: "Login Successful"
-//             });
-//         }
-//     })(req, res, next);
-// });
-
-router.post('/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    res.json({
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.json({
+      success:true,
+      message: 'Login Failed' });
+     }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.json({
         success:true,
-        message: 'Login seuccessful'
+        message: 'Login seuccessful'});
     });
-  }
-);
+  })(req, res, next);
+});
+
 
 
 
 router.get('/logout', function(req, res){
-    req.logout();
-    res.json({
-        success:true,
-        message: 'Logout seuccessful'
-    });
+    if(req.isAuthenticated()){
+      req.logout();
+      res.json({
+          success:true,
+          message: 'Logout seuccessful'
+      });
+    }
   });
+
+  
 
 module.exports = router;
