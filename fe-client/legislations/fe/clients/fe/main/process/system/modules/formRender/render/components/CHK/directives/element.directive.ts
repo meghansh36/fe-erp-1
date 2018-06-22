c56b499893e0e724e480436b6424284948e4d9e8
Chirag <Chirag2120787@gmail.com>
2018-06-22 18:55:27 +0530

    checkbox componenet added

---
+++ fe-client/legislations/fe/clients/fe/main/process/system/modules/formRender/render/components/CHK/directives/element.directive.ts
@@ -0,0 +1,2 @@
+import { LgCheckboxElementDirective } from '@L2Process/system/modules/formRender/render/components/CHK/directives/element.directive';
+export class CheckboxElementDirective extends LgCheckboxElementDirective{}
---
+++ fe-client/legislations/fe/main/process/system/modules/formRender/render/components/CHK/btn.component.ts
@@ -0,0 +1,2 @@
+import { FeCHKComponent } from '@L1Process/system/modules/formRender/render/components/CHK/chk.component';
+export class LgCHKComponent extends FeCHKComponent{}
