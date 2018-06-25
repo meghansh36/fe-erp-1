58b8a4b0287888a8453883f9730863f6302abf5c
Shubham Verma <maveric.shubham@gmail.com>
2018-06-25 11:16:39 +0530

    Form Generator Changes

---
+++ fe-client/main/forms/DefaultForm.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+
+import { FeFormSchemaService } from '../services/formSchema.service';
+import { FeFormComponent } from '@L1Process/system/modules/formGenerator/components/feForm/feForm.component';
+
+export class DefaultFormComponent implements OnInit, AfterViewInit  {
+    @ViewChild(FeFormComponent) form: FeFormComponent;
+    protected config = [];
+    constructor(protected formSchemaService: FeFormSchemaService) {}
+
+    protected code: String = 'DEFAULTFORM';
+    ngOnInit() {
+        const formSchema = this.formSchemaService.getFormSchema(this.code);
+        const formComponents = formSchema.components;
+        this.config = formSchema;
+    }
+
+    ngAfterViewInit() {
+        let previousValid = this.form.valid;
+        this.form.changes.subscribe(() => {
+            if (this.form.valid !== previousValid) {
+                previousValid = this.form.valid;
+                this.form.setDisabled('submit', !previousValid);
+            }
+        });
+
+        this.form.setDisabled('submit', true);
+        //this.form.setValue('input', 'Shubham Verma');
+    }
+
+    submit(value: {[name: string]: any}) {
+        console.log(value);
+    }
+}
---
+++ fe-client/main/process/system/modules/formGenerator/directives/feField/feField.directive.ts
@@ -0,0 +1,54 @@
+import { ComponentFactoryResolver, ComponentRef, Directive, Input, OnChanges, OnInit, Type, ViewContainerRef } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+import { FeButtonComponent } from '../../components/feButton/feButton.component';
+import { FeTextComponent } from '../../components/feText/feText.component';
+import { FeSelectComponent } from '../../components/feSelect/feSelect.component';
+
+import { Field } from '../../models/field.interface';
+import { FieldConfig } from '../../models/field-config.interface';
+
+const components: {[type: string]: Type<Field>} = {
+  BTN: FeButtonComponent,
+  TXT: FeTextComponent,
+  SEL: FeSelectComponent
+};
+
+@Directive({
+  selector: '[feField]'
+})
+export class FeFieldDirective implements Field, OnChanges, OnInit {
+  @Input()
+  config: FieldConfig;
+
+  @Input()
+  group: FormGroup;
+
+  component: ComponentRef<Field>;
+
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private container: ViewContainerRef
+  ) {}
+
+  ngOnChanges() {
+    if (this.component) {
+      this.component.instance.config = this.config;
+      this.component.instance.group = this.group;
+    }
+  }
+
+  ngOnInit() {
+    if (!components[this.config.type]) {
+      const supportedTypes = Object.keys(components).join(', ');
+      throw new Error(
+        `Trying to use an unsupported type (${this.config.type}).
+        Supported types: ${supportedTypes}`
+      );
+    }
+    const component = this.resolver.resolveComponentFactory<Field>(components[this.config.type]);
+    this.component = this.container.createComponent(component);
+    this.component.instance.config = this.config;
+    this.component.instance.group = this.group;
+  }
+}
