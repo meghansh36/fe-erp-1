1402bc3c32b77fd27f6f44e34dbeef7b9214db3a
Chirag <Chirag2120787@gmail.com>
2018-07-05 10:43:06 +0530

    path

--- fe-server/main/process/default/models/index.js
+++ fe-server/main/process/default/models/index.js
@@ -4,7 +4,10 @@ var Sequelize = require('sequelize');
 //var basename  = path.basename(__filename);
 var db        = {};
 //var CONFIG = require('/Users/chiragbansal/Desktop/FlexiEle-Angular/fe-erp/config/config.js');
-var CONFIG = require('../../../../../config/config.js');
+var CONFIG = require(path.join(path.relative(__dirname,process.env.L0),'/config/config.js'));
+console.log(path.join(path.relative(__dirname,process.env.L0),'/config/config.js'));  
+//console.log(__dirname);
+//console.log("asdsdasdsasaddsd:=>  ",);
 const CLASSMETHODS = 'classMethods';
 const ASSOCIATE = 'associate';
 
@@ -25,9 +28,8 @@ fs
     return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-      console.log(file);
     var model = sequelize['import'](path.join(__dirname, file));
-    console.log(model);
+//    console.log(model);
     db[model.name] = model;
     //model.sync({force:false});
   });
