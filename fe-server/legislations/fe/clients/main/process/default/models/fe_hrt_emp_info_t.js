fd85b91264653cd01afe71a9234c9aa77cf07daa
Chirag <Chirag2120787@gmail.com>
2018-07-09 12:18:35 +0530

    model inheritance

--- fe-server/legislations/fe/clients/main/process/default/models/fe_hrt_emp_info_t.js
+++ fe-server/legislations/fe/clients/main/process/default/models/fe_hrt_emp_info_t.js
@@ -1,8 +1,8 @@
 /* jshint indent: 1 */
-const LgEmpInfoModel = require(process.env.L2Process+'default/models/fe_hrt_emp_info_t.js')
-
-module.exports =  class EmpInfoModel extends LgEmpInfoModel {
+//const Sequelize = require('sequelize');
+const LgEmpInfoModel2 = require(process.env.L2Process+'default/models/fe_hrt_emp_info_t.js')
+module.exports =  class EmpInfoModel extends LgEmpInfoModel2 {
 	constructor(){
-		super();
+		super(...arguments);
 	}
 }	
--- fe-server/main/process/default/routes/aureole_lookup.js
+++ fe-server/main/process/default/routes/aureole_lookup.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 
-//router.get('/', aureole_lookup.findAll);
+router.get('/', aureole_lookup.findAll);
 
 
 module.exports = router;
